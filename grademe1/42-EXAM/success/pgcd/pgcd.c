0060f9095e934f87d4519d4a8a21d6b1748dafce
larocqueg <portugalgabriel2021@gmail.com>
2024-12-09 12:56:35 +0000

    100/100

---
+++ grademe1/42-EXAM/success/pgcd/pgcd.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(int ac, char **av)
+{
+	int	n1;
+	int	n2;
+	if (ac == 3)
+	{
+		n1 = atoi(av[1]);
+		n2 = atoi(av[2]);
+		if (n1 > 0 && n2 > 0)
+		{
+			while (n1 != n2)
+			{
+				if (n1 > n2)
+					n1 -= n2;
+				else n2 -= n1;
+			}
+			printf("%i\n", n1);
+			return (0);
+		}
+	}
+	printf("\n");
+	return (1);
+}
