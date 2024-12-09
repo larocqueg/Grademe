8b5dca423d94afc9ba613c1e476f47eb1bf52736
larocqueg <portugalgabriel2021@gmail.com>
2024-12-09 15:21:22 +0000

    75/100

---
+++ Grademe2/success/rot_13/rot_13.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int ac, char **av)
+{
+	int	i = 0;
+	if (ac == 2)
+	{
+		while (av[1][i])
+		{
+			if ((av[1][i] >= 'A' && av[1][i] <= 'M') ||
+				(av[1][i] >= 'a' && av[1][i] <= 'm'))
+				ft_putchar(av[1][i] + 13);
+			else if ((av[1][i] > 'M' && av[1][i] <= 'Z') ||
+				av[1][i] > 'm' && av[1][i] <= 'z')
+				ft_putchar(av[1][i] -13);
+			else
+				ft_putchar(av[1][i]);
+			i++;
+		}
+		ft_putchar('\n');
+		return (0);
+	}
+	ft_putchar('\n');
+	return (1);
+}
---
+++ Grademe3/success/ft_atoi/ft_atoi.c
@@ -0,0 +1,18 @@
+int	ft_atoi(const char *str)
+{
+	int	i = 0;
+	int res = 0;
+	int sign = 1;
+
+	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+		res = res * 10 + (str[i++] - '0');
+	return (res * sign);
+}
