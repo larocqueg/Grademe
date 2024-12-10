1b147b8a1d4535e308f4ec94aa848250e549ad28
larocqueg <portugalgabriel2021@gmail.com>
2024-12-10 15:04:46 +0000

    100/100

---
+++ Rank02/Grademe7/success/camel_to_snake/camel_to_snake.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+int	main(int ac, char **av)
+{
+	int i = 0;
+	char	temp;
+
+	if (ac == 2)
+	{
+		while (av[1][i])
+		{
+			if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+			{
+				if (i > 0)
+					write(1, "_", 1);
+				temp = av[1][i] + 32;
+				write(1, &temp, 1);
+			}
+			else
+				write(1, &av[1][i], 1);
+			i++;
+		}
+		write(1, "\n", 1);
+		return (0);
+	}
+	write(1, "\n", 1);
+	return (1);
+}
