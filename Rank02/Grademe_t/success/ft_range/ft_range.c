532cdbad84bc0384633cf93b6c04ffe41b5b8468
Gabriel Aguiar De La Rocque <gde-la-r@c2r2s8.42porto.com>
2024-12-13 11:47:58 +0000

    100/100

---
+++ Rank02/Grademe_t/success/ft_range/ft_range.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+
+int	*ft_range(int start, int end)
+{
+	int *dest;
+	int range;
+	int i = 0;
+
+	if (start > end)
+	{
+		range = start - end;
+		dest = (int *)malloc(sizeof(int) * range);
+		if (!dest)
+			return (0);
+		while (start >= end)
+			dest[i++] = start--;
+		return (dest);
+	}
+	range = end - start;
+	dest = (int *)malloc(sizeof(int) * range);
+	if (!dest)
+		return (0);
+	while (start <= end)
+		dest[i++] = start++;
+	return (dest);
+}
